3dfcbf2d53e63cfae0ef45e4ef49750248a480e0
Danilo Ramos <125937280+daniloramosbr@users.noreply.github.com>
2023-06-01 14:11:27 -0300

    tabuada

---
+++ tabuada.js
@@ -0,0 +1,17 @@
+function tabuada() {
+let num  = document.getElementById('txtn')
+let sel = document.getElementById('txts')
+if (num.value.length == 0 ) {
+    window.alert('erro faltam numeros')
+} else {
+    let n = Number(num.value)
+    sel.innerHTML = ''
+    let c = 1
+    while (c <= 10) {
+        let item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c} `
+        sel.appendChild(item)
+        c++
+        }
+    }
+}
